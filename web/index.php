524e3322fd3b340d2402348b758802290f21fe11
mr-farai <faraimadzima@farai-2.local>
2016-11-27 00:30:26 +0200

    boom

--- web/index.php
+++ web/index.php
@@ -227,4 +227,46 @@
 
     //// Write output to file // DEBUG
     file_put_contents( $filename, $output ); // DEBUG
+
+    $data = array(
+          // Merchant details
+          'merchant_id' => '',
+          'merchant_key' => '',
+          'return_url' => 'http://www.yourdomain.co.za/thank-you.html',
+          'cancel_url' => 'http://www.yourdomain.co.za/cancelled-transction.html',
+          'notify_url' => 'http://www.yourdomain.co.za/itn.php',
+	          'name_first' => 'First Name',
+	          'name_last'  => 'Last Name',
+	          'email_address'=> 'valid@email_address.com',
+	          'm_payment_id' => '8542', //Unique payment ID to pass through to notify_url
+	          'amount' => number_format( sprintf( ".2f", $cartTotal ), 2, '.', '' ), //Amount needs to be in ZAR,
+          if you have a multicurrency system, the conversion needs to place before building this array
+          'item_name' => 'Item Name',
+          'item_description' => 'Item Description',
+          'custom_int1' => '9586', //custom integer to be passed through
+          'custom_str1' => 'custom string to be passed through with the transaction to the notify_url page'
+          );
+
+      // Create GET string
+      foreach( $data as $key => $val )
+      {
+          if(!empty($val))
+          {
+          	$pfOutput .= $key .'='. urlencode( trim( $val ) ) .'&';
+          }
+  	}
+      // Remove last ampersand
+      $getString = substr( $pfOutput, 0, -1 );
+      if( isset( $passPhrase ) )
+      {
+          $getString .= '&passphrase='.$passPhrase;
+      }
+      $data['signature'] = md5( $getString );
+
+      // If in testing mode use the sandbox domain ?  sandbox.payfast.co.za else www.payfast.co.za
+       $testingMode = true;
+       $pfHost = $testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
+       $htmlForm = '
+ <form action="https://'.$pfHost.'/eng/process" method="post">'; foreach($data as $name=> $value) { $htmlForm .= '<input name="'.$name.'" type="hidden" value="'.$value.'" />'; } $htmlForm .= '<input type="submit" value="Pay Now" /></form>'; echo $htmlForm;
+
     ?>
