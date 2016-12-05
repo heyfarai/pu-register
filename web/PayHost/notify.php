2ad607e7d472b3532e32a1334efabe1d7dbd0d92
mr-farai <faraimadzima@farai-2.local>
2016-12-05 13:03:34 +0200

    after heroku pull

--- web/PayHost/notify.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/*
-	 * File to receive Notify from PayGate
-	 * NOTE: File must be made accessible by www
-	 */
-
-	error_log(file_get_contents('php://input'));
-	file_put_contents("php://stderr", $_POST);
-	echo 'OK';
