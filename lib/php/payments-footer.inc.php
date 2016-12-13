88cd2b8d5997a20ef30373291843b7eb6b7d7e1d
mr-farai <faraimadzima@farai-2.local>
2016-12-13 19:59:57 +0200

    Added terms

--- lib/php/payments-footer.inc.php
+++ lib/php/payments-footer.inc.php
@@ -1,6 +1,6 @@
             <div class="payments-footer">
                 <img width="260" src="/img/payment-option-credit-card.png" />
                 <p>
-                    All payments processed by securely by PayGate.
+                    All payments processed by securely by PayGate. <a href="https://pixelup.co.za/terms">Terms and Conditions</a>
                 </p>
             </div>
