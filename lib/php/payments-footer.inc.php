b1491c9db542b8a5207361b3570c421c346fc9b4
mr-farai <faraimadzima@farai-2.local>
2016-12-14 10:10:25 +0200

    card icons offsite

--- lib/php/payments-footer.inc.php
+++ lib/php/payments-footer.inc.php
@@ -1,5 +1,5 @@
             <div class="payments-footer">
-                <img width="300" src="/img/payment-option-credit-card.png" />
+                <img width="300" src="https://pixelup.co.za/img/payment-option-credit-card.png" />
                 <p>
                     All payments processed by securely by PayGate. <a target="_blank" href="https://pixelup.co.za/terms">Terms and Conditions</a>
                 </p>
