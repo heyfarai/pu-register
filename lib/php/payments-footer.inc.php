4064315f04a6945782be7afc320aa429c95a66f5
mr-farai <faraimadzima@farai-2.local>
2016-12-21 15:41:25 +0200

    added terms and privacy details

--- lib/php/payments-footer.inc.php
+++ lib/php/payments-footer.inc.php
@@ -1,6 +1,8 @@
             <div class="payments-footer">
                 <img width="300" height="19" src="https://pixelup.co.za/img/payment-option-credit-card.png" />
                 <p>
-                    All payments processed by securely by PayGate. <a target="_blank" href="https://pixelup.co.za/terms">Terms and Conditions</a>
+                    All payments processed by securely by PayGate.
                 </p>
+                <p><a target="_blank" href="https://pixelup.co.za/terms">Terms and Conditions</a> <a target="_blank" href="https://pixelup.co.za/privacy">Privacy Policy</a></p>
+                <small>PIXEL UP! events and this website are run by EFFED UP! a private company based in South Africa with registration number 2016/470802/07. Our address is 11 Guildford Manor, 7 Hunter Close, Douglasdale, Sandton, Johannesburg, 2191, South Africa.</small>
             </div>
