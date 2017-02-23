dedb7a6a4bfe5d24d368eed1d1ccef21e243d1b4
mr-farai <faraimadzima@farai-2.local>
2017-02-23 17:32:03 +0200

    removed log

--- lib/php/header.inc.php
+++ lib/php/header.inc.php
@@ -36,10 +36,8 @@
                 window.addEventListener('load', function() {
                     navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
                         // Registration was successful
-                        // console.log('ServiceWorker registration successful with scope: ', registration.scope);
                     }).catch(function(err) {
                         // registration failed :(
-                        // console.log('ServiceWorker registration failed: ', err);
                     });
                 });
             }
